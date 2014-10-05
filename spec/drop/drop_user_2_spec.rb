016e668c9bf0a61978753ca88246c4f3048c3b14
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-05 23:52:02 +0900

    Add drop spec

---
+++ spec/drop/drop_user_2_spec.rb
@@ -0,0 +1,77 @@
+describe 'Gratan::Client#apply' do
+  before(:each) do
+    apply {
+      <<-RUBY
+user 'scott', 'localhost', identified: 'tiger', required: 'SSL' do
+  on '*.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+  end
+
+  on 'test.*' do
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+
+  on 'mysql.user' do
+    grant 'SELECT (user)'
+  end
+end
+
+user 'bob', 'localhost' do
+  on '*.*' do
+    grant 'USAGE'
+  end
+
+  on 'test.*' do
+    grant 'ALL PRIVILEGES'
+  end
+end
+      RUBY
+    }
+  end
+
+  context 'when drop user' do
+    subject { client(dry_run: true) }
+
+    it do
+      apply(subject) {
+      <<-RUBY
+user 'bob', 'localhost' do
+  on '*.*' do
+    grant 'USAGE'
+  end
+
+  on 'test.*' do
+    grant 'ALL PRIVILEGES'
+  end
+end
+        RUBY
+      }
+
+      expect(show_grants).to match_array [
+        "GRANT ALL PRIVILEGES ON `test`.* TO 'bob'@'localhost'",
+        "GRANT SELECT (user) ON `mysql`.`user` TO 'scott'@'localhost'",
+        "GRANT SELECT, INSERT ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40' REQUIRE SSL",
+        "GRANT UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
+        "GRANT USAGE ON *.* TO 'bob'@'localhost'",
+      ]
+    end
+  end
+
+  context 'when drop all users' do
+    subject { client(dry_run: true) }
+
+    it do
+      apply(subject) { '' }
+
+      expect(show_grants).to match_array [
+        "GRANT ALL PRIVILEGES ON `test`.* TO 'bob'@'localhost'",
+        "GRANT SELECT (user) ON `mysql`.`user` TO 'scott'@'localhost'",
+        "GRANT SELECT, INSERT ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40' REQUIRE SSL",
+        "GRANT UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
+        "GRANT USAGE ON *.* TO 'bob'@'localhost'",
+      ]
+    end
+  end
+end
