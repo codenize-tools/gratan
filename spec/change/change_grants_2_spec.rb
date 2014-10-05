4e338f2c16838cb289b35605fb0ece8c2f69658f
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-05 23:14:03 +0900

    Add change spec

---
+++ spec/change/change_grants_2_spec.rb
@@ -0,0 +1,129 @@
+describe 'Gratan::Client#apply' do
+  before(:each) do
+    apply(subject) {
+      <<-RUBY
+user 'scott', 'localhost', identified: 'tiger', required: 'SSL' do
+  on '*.*' do
+    grant 'USAGE'
+  end
+end
+
+user 'bob', 'localhost' do
+  on '*.*', with: 'GRANT OPTION' do
+    grant 'ALL PRIVILEGES'
+  end
+end
+      RUBY
+    }
+  end
+
+  context 'when update password' do
+    subject { client }
+
+    it do
+      apply(subject) {
+        <<-RUBY
+user 'scott', 'localhost', identified: '123', required: 'SSL' do
+  on '*.*' do
+    grant 'USAGE'
+  end
+end
+
+user 'bob', 'localhost', identified: '456' do
+  on '*.*', with: 'GRANT OPTION' do
+    grant 'ALL PRIVILEGES'
+  end
+end
+        RUBY
+      }
+
+      expect(show_grants).to match_array [
+        "GRANT ALL PRIVILEGES ON *.* TO 'bob'@'localhost' IDENTIFIED BY PASSWORD '*531E182E2F72080AB0740FE2F2D689DBE0146E04' WITH GRANT OPTION",
+        "GRANT USAGE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*23AE809DDACAF96AF0FD78ED04B6A265E05AA257' REQUIRE SSL",
+      ]
+    end
+  end
+
+  context 'when remove password' do
+    subject { client }
+
+    it do
+      apply(subject) {
+        <<-RUBY
+user 'scott', 'localhost', identified: nil, required: 'SSL' do
+  on '*.*' do
+    grant 'USAGE'
+  end
+end
+
+user 'bob', 'localhost' do
+  on '*.*', with: 'GRANT OPTION' do
+    grant 'ALL PRIVILEGES'
+  end
+end
+        RUBY
+      }
+
+      puts show_grants
+
+      expect(show_grants).to match_array [
+        "GRANT ALL PRIVILEGES ON *.* TO 'bob'@'localhost' WITH GRANT OPTION",
+        "GRANT USAGE ON *.* TO 'scott'@'localhost' REQUIRE SSL",
+      ]
+    end
+  end
+
+  context 'when skip update password' do
+    subject { client }
+
+    it do
+      apply(subject) {
+        <<-RUBY
+user 'scott', 'localhost', required: 'SSL' do
+  on '*.*' do
+    grant 'USAGE'
+  end
+end
+
+user 'bob', 'localhost' do
+  on '*.*', with: 'GRANT OPTION' do
+    grant 'ALL PRIVILEGES'
+  end
+end
+        RUBY
+      }
+
+      expect(show_grants).to match_array [
+        "GRANT ALL PRIVILEGES ON *.* TO 'bob'@'localhost' WITH GRANT OPTION",
+        "GRANT USAGE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40' REQUIRE SSL",
+      ]
+    end
+  end
+
+  context 'when update require' do
+    subject { client }
+
+    it do
+      apply(subject) {
+        <<-RUBY
+user 'scott', 'localhost', required: 'X509' do
+  on '*.*' do
+    grant 'USAGE'
+  end
+end
+
+user 'bob', 'localhost', required: 'SSL' do
+  on '*.*', with: 'GRANT OPTION' do
+    grant 'ALL PRIVILEGES'
+  end
+end
+        RUBY
+      }
+
+      expect(show_grants).to match_array [
+        "GRANT ALL PRIVILEGES ON *.* TO 'bob'@'localhost' REQUIRE SSL WITH GRANT OPTION",
+        "GRANT USAGE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40' REQUIRE X509",
+      ]
+    end
+  end
+end
