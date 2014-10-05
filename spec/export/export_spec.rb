9c7605fcf7d78942bb095a2d35c0b1acb9d6c904
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-06 01:02:28 +0900

    Add spec

--- spec/export/export_spec.rb
+++ spec/export/export_spec.rb
@@ -92,4 +92,28 @@ end
       RUBY
     end
   end
+
+  context 'when with option exists' do
+    let(:grantfile) {
+      <<-RUBY
+user "scott", "%" do
+  on "*.*", :with=>"GRANT OPTION" do
+    grant "USAGE"
+  end
+end
+      RUBY
+    }
+
+    subject { client(ignore_user: /\Abob\z/) }
+
+    before do
+      apply(subject) do
+        grantfile
+      end
+    end
+
+    it do
+      expect(subject.export.strip).to eq grantfile.strip
+    end
+  end
 end
