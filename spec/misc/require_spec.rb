33219f57946761dac4d8bad329a607829345db2d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-06 00:55:52 +0900

    Add spec

---
+++ spec/misc/require_spec.rb
@@ -0,0 +1,77 @@
+describe 'Gratan::Client#apply' do
+  context 'when load group file' do
+    subject { client }
+
+    it do
+      dsl = <<-RUBY
+user 'scott', 'localhost', identified: 'tiger' do
+  on '*.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+
+  on 'test.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+end
+      RUBY
+
+      tempfile(dsl) do |f|
+        apply(subject) {
+          <<-RUBY
+require 'time'
+require '#{f.path}'
+          RUBY
+        }
+      end
+
+      expect(show_grants).to match_array [
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40'",
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
+      ]
+    end
+  end
+
+  context 'when load group file (.rb)' do
+    subject { client }
+
+    it do
+      dsl = <<-RUBY
+user 'scott', 'localhost', identified: 'tiger' do
+  on '*.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+
+  on 'test.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+end
+      RUBY
+
+      tempfile(dsl, ext: '.rb') do |f|
+        apply(subject) {
+          <<-RUBY
+require 'time'
+require '#{f.path.sub(/\.rb\z/, '')}'
+          RUBY
+        }
+      end
+
+      expect(show_grants).to match_array [
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40'",
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
+      ]
+    end
+  end
+end
