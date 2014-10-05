b38229f8e7462ef0e2278c8acddb2d9e47ae3b27
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-06 00:44:36 +0900

    Add debug log spec

--- spec/misc/misc_spec.rb
+++ spec/misc/misc_spec.rb
@@ -34,4 +34,41 @@ end
       ]
     end
   end
+
+  context 'when set debug' do
+    let(:logger) do
+      logger = Gratan::Logger.instance
+      logger.set_debug(true)
+      logger
+    end
+
+    subject { client(logger: logger) }
+
+    it do
+      apply(subject) {
+        <<-RUBY
+user 'scott', 'localhost', identified: 'tiger' do
+  on '*.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+
+  on 'test.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+end
+        RUBY
+      }
+
+      expect(show_grants).to match_array [
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40'",
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
+      ]
+    end
+  end
 end
