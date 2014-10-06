4cb8044310093de7ea7772d024841f696a4eb33d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-06 23:29:32 +0900

    Fix spec

--- spec/misc/misc_spec.rb
+++ spec/misc/misc_spec.rb
@@ -37,8 +37,12 @@ end
 
   context 'when set debug' do
     let(:logger) do
-      logger = Gratan::Logger.instance
+      logger = Gratan::Logger.send(:new)
       logger.set_debug(true)
+      expect(logger).to receive(:debug).with("[DEBUG] SET SQL_LOG_BIN = 0")
+      expect(logger).to receive(:debug).with("[DEBUG] SELECT user, host FROM mysql.user")
+      expect(logger).to receive(:info).with("GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost' IDENTIFIED BY 'tiger'")
+      expect(logger).to receive(:info).with("GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost' IDENTIFIED BY 'tiger'")
       logger
     end
 
