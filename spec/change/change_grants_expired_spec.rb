a2cb652d66bb3d50fac27bb369062a220062f737
Genki Sugawara <sugawara@cookpad.com>
2014-10-07 12:45:57 +0900

    Fix spec

--- spec/change/change_grants_expired_spec.rb
+++ spec/change/change_grants_expired_spec.rb
@@ -64,4 +64,49 @@ end
       ]
     end
   end
+
+  context 'when object has not expired' do
+    let(:logger) do
+      logger = Logger.new('/dev/null')
+      expect(logger).to receive(:warn).with('[WARN] User `scott@localhost`: Object `test.*` has expired')
+      logger
+    end
+
+    subject do
+      client(
+        enable_expired: true,
+        logger: logger
+      )
+    end
+
+    it do
+      dsl = <<-RUBY
+user 'scott', 'localhost', required: 'SSL' do
+  on '*.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+  end
+
+  on 'test.*', expired: '2014/10/06' do
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+
+  on 'mysql.user' do
+    grant 'SELECT (user)'
+  end
+end
+      RUBY
+
+      Timecop.freeze(Time.parse('2014/10/05 23:59:59')) do
+        apply(subject) { dsl }
+      end
+
+      expect(show_grants).to match_array [
+        "GRANT SELECT (user) ON `mysql`.`user` TO 'scott'@'localhost'",
+        "GRANT SELECT, INSERT ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40' REQUIRE SSL",
+        "GRANT SELECT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
+      ]
+    end
+  end
 end
