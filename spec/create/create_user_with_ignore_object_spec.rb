8a033aa46bf3aaaab5f113532f99b3af6fb76736
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-11-05 20:49:54 +0900

    Add spec

--- spec/create/create_user_with_ignore_object_spec.rb
+++ spec/create/create_user_with_ignore_object_spec.rb
@@ -40,4 +40,37 @@ end
       ]
     end
   end
+
+  context 'when create user with ignore_object (2)' do
+    subject { client(ignore_object: /test2/) }
+
+    it do
+      result = apply(subject) {
+        <<-RUBY
+user 'scott', 'localhost', identified: 'tiger' do
+  on '*.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+
+  on 'test.*' do
+    grant 'SELECT'
+    grant 'INSERT'
+    grant 'UPDATE'
+    grant 'DELETE'
+  end
+end
+        RUBY
+      }
+
+      expect(result).to be_truthy
+
+      expect(show_grants).to match_array [
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40'",
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
+      ]
+    end
+  end
 end
